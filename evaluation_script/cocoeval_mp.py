31f14456e5a8214de731256552f850ac82854870
Ziyu Liu <113190597+Liuziyu77@users.noreply.github.com>
2024-03-22 17:23:53 +0800

    Update cocoeval_mp.py

--- evaluation_script/cocoeval_mp.py
+++ evaluation_script/cocoeval_mp.py
@@ -11,11 +11,17 @@ from mmengine.logging import MMLogger
 from pycocotools.cocoeval import COCOeval, Params
 from tqdm import tqdm
 
+def get_curr_working_dir():
+    curr_working_dir = os.getcwd()
+    return curr_working_dir
+    
+current_working_directory = get_curr_working_dir()
+annotation_file_path = "{}/annotations/v3det_2023_v1_val.json".format(current_working_directory)
 
 class COCOevalMP(COCOeval):
 
     def __init__(self, cocoGt=None, cocoDt=None, iouType='bbox', num_proc=8,
-                 tree_ann_path='data/V3Det/annotations/v3det_2023_v1_category_tree.json'):
+                 tree_ann_path = annotation_file_path):
         '''
         Initialize CocoEval using coco APIs for gt and dt
         :param cocoGt: coco object with ground truth annotations
